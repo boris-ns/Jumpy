43a7c1028be5af8a74ef5fb5149268c921e3a6fc
Boris S <b.sulicenko@gmail.com>
2016-03-01 23:33:51 +0100

    Changed regular for loop with foreach

--- src/handlers/BlockHandler.java
+++ src/handlers/BlockHandler.java
@@ -17,20 +17,12 @@ public class BlockHandler
 	
 	public void render(Graphics g)
 	{
-		for(int i = 0; i < blocks.size(); i++)
-		{
-			blocks.get(i).render(g);
-		}
-	}
-	
-	public void addBlock(Block block)
-	{
-		blocks.add(block);
-	}
-	
-	public void removeBlock(int i)
-	{
-		blocks.remove(i);
+//		for(int i = 0; i < blocks.size(); i++)
+//		{
+//			blocks.get(i).render(g);
+//		}
+		for(Block block : blocks)
+			block.render(g);
 	}
 	
 //	public void levelDesign()
