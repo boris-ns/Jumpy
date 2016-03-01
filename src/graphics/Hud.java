c50b13a79f506cce638521449f8fb8b3a238408f
Boris S <b.sulicenko@gmail.com>
2016-03-01 23:34:25 +0100

    Added hud class

---
+++ src/graphics/Hud.java
@@ -0,0 +1,18 @@
+package graphics;
+
+import java.awt.Graphics;
+
+public class Hud 
+{	
+	private int coinsCollected;
+	
+	public void tick(int coins)
+	{
+		coinsCollected = coins;
+	}
+	
+	public void render(Graphics g)
+	{
+		
+	}
+}
