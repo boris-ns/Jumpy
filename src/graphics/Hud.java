e982a64a20141146ba6d33cd1bd125ec048bfe90
boris-ns <b.sulicenko@gmail.com>
2016-07-24 22:12:40 +0200

    Added Level text

--- src/graphics/Hud.java
+++ src/graphics/Hud.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
+import main.Game;
+
 // Klasa Heads up Display (HUD) sluzi da obavestava igraca o njegovim heltima i sakupljenim novcicima
 public class Hud 
 {	
@@ -19,13 +21,21 @@ public class Hud
 	// Iscrtava i ispisuje na ekran dobijene vrednosti iz tick() metode
 	public void render(Graphics g)
 	{
+		// Iscrtavanje Health bar-a
 		g.setColor(Color.RED);
 		g.fillRect(15, 15, playerHealth, 12);
 		
+		// Coins collected
 		g.setFont(new Font("Arial Black", 1, 18));
 		g.setColor(Color.BLACK);
 		g.drawString("Coins: " + coinsCollected, 17, 52);
 		g.setColor(Color.ORANGE);
 		g.drawString("Coins: " + coinsCollected, 15, 50);
+		
+		// Level
+		g.setColor(Color.BLACK);
+		g.drawString("Level: " + Game.level, 17, 77);
+		g.setColor(Color.ORANGE);
+		g.drawString("Level: " + Game.level, 15, 75);
 	}
 }
